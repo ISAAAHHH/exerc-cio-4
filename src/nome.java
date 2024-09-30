23f7dd2b2b037d4c29bba8420c8ba1cbde4405c2
isabele <isabele.assumpcao@escola.pr.gov.br>
2024-09-30 11:37:46 -0300

    iniciamos os arquivos

---
+++ src/nome.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class nome {
+    public static void main(String[] args) {
+           Scanner leitura = new Scanner(System.in);
+            System.out.println("digite seu nome: ");
+            String nome = leitura.nextLine();
+        System.out.printf(nome);
+
+        
+
+    }
+}
